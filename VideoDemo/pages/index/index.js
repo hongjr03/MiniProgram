0eaadf019b572a56b08037e218f7ca9982014e57
hongjr03 <hongjr03@gmail.com>
2024-08-26 21:48:07 +0800

    弹幕实现

--- VideoDemo/pages/index/index.js
+++ VideoDemo/pages/index/index.js
@@ -1,3 +1,13 @@
+function getRandomColor() {
+  const rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length === 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+
 Page({
   /**
    * 页面的初始数据
@@ -29,12 +39,51 @@ Page({
           "http://arch.ahnu.edu.cn/__local/5/DA/BD/7A27865731CF2B096E90B522005_A29CB142_6525BCF.mp4?e=.mp4",
       },
     ],
+    src: "",
+    danmuList: [
+      {
+        text: "第 1s 出现的弹幕",
+        color: "#ff0000",
+        time: 1,
+      },
+      {
+        text: "第 3s 出现的弹幕",
+        color: "#ff00ff",
+        time: 3,
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+  onLoad: function (options) {
+    this.videoCtx = wx.createVideoContext('myVideo');
+  },
+
+  /**
+   * 播放视频
+   */
+  playVideo: function (e) {
+    // console.log(e);
+    this.videoCtx.stop();
+    this.setData({
+      src: e.currentTarget.dataset.url,
+    })
+    // console.log(this.data.src);
+    this.videoCtx.play();
+  },
+  
+  inputDanmu: function (e) {
+    this.inputValue = e.detail.value;
+  },
+
+  sendDanmu: function () {
+    this.videoCtx.sendDanmu({
+      text: this.inputValue,
+      color: getRandomColor()
+    })
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
